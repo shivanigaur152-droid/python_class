146e26bff1527a94b21d820a91ccdc0e86131997
Shivani <shivanigaur152@gmail.com>
2026-06-22 12:50:26 +0530

    second commit

--- python_ass_1st/assigment_1st.py
+++ python_ass_1st/assigment_1st.py
@@ -1,2 +1,15 @@
 for i in range(1,10):
   print(i)
+
+  #que.2
+  total=0
+  for i in range(1,101):
+    total=total+i
+  print(total)
+
+  #table
+  total=0
+  for i in range(1,11):
+    table=15*i
+    print(table)
+
